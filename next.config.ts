9fd4a8c9b95d38dc14e0e440ef2a1c1948285ff7
Rahman <cybertank378@gmail.com>
2025-03-14 01:14:17 +0700

    Remove unused Prisma migrations and cleanup project files
    Deleted obsolete Prisma migrations related to database schema changes and removed an unnecessary tree.txt file. This cleanup reduces clutter and ensures the repository only contains relevant and active files.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const nextConfig: NextConfig = {
+    env: {
+        JWT_SECRET: process.env.JWT_SECRET,
+    },
     trailingSlash: false,
     basePath: "",
     reactStrictMode: true, // Enforce React best practices
